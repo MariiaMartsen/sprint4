884a54c1b93fbf2efc37829128bd1f37412b6636
Mariia Martsen <martsen@yandex-team.ru>
2022-03-29 20:04:21 +0300

    add checkUserName test

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -5,6 +5,7 @@ import io.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class Mesto1Test {
 
@@ -62,6 +63,16 @@ public class Mesto1Test {
                 .auth().oauth2(bearerToken) // Передаём токен для аутентификации
                 .delete("/api/cards/{photoId}/likes", photoId) // Делаем DELETE-запрос
                 .then().assertThat().statusCode(200); // Проверяем, что сервер вернул код 200
+    }
+
+    @Test
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
     }
 
 }
